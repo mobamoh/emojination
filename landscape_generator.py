fcb9f0145e6c395e76952396447aa43f780530dc
Mohamed Bamoh <mohamed.bamoh@mb-1626.fritz.box>
2023-09-22 20:53:42 +0200

    added functions and user input logic

--- landscape_generator.py
+++ landscape_generator.py
@@ -1,7 +1,32 @@
-cols = 12
-rows = 30
-total = cols * rows
+import random
 
-message = f"we want to generate a landscape which is {cols} by {rows}"
+def generate_land(cols=10,rows=10):
+    total = cols * rows
+    print(f"generate a landscape which is {cols} by {rows}")
+    items = ["a","b","c","d","e","f"]
+    for row in range(rows):
+        strCol = ""
+        for col in range(cols):
+            strCol += random.choice(items)
+        print(strCol)
 
-print(message)
+
+def ask_input_number(question):
+    tries = 0
+    while tries < 3:
+        num = input(question)
+        if question == "quit":
+            quit()
+        elif str.isnumeric(num):
+            return int(num)
+        else:
+            print("please enter a number!")
+            tries+=1
+    print("you are not serious!!")
+    quit()
+
+
+cols = ask_input_number("enter the number of columns: ")
+rows = ask_input_number("enter the number of rows: ")
+
+generate_land(cols,rows)
